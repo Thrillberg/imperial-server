9d40adbd6bf05de81e48636c48656a0dd371293d
Eric Tillberg <eric.tillberg@gmail.com>
2020-09-06 17:35:38 -0400

    Make the proof of concept work

--- imperial.go
+++ imperial.go
@@ -21,6 +21,20 @@ type RegisteredPlayersOutput struct {
 	} `json:"payload"`
 }
 
+type ActionData struct {
+	Type    string `json:"type"`
+	Payload struct {
+		Action string `json:"action"`
+	} `json:"payload"`
+}
+
+type GameLogOutput struct {
+	Type    string `json:"type"`
+	Payload struct {
+		GameLog []string `json:"gameLog"`
+	} `json:"payload"`
+}
+
 var upgrader = websocket.Upgrader{
 	CheckOrigin:     func(*http.Request) bool { return true },
 	ReadBufferSize:  1024,
@@ -33,6 +47,7 @@ var payload map[string][]string
 
 var connections = map[int]*websocket.Conn{}
 var players = map[int]string{}
+var gameLog = []string{}
 var nextId = 1
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -52,18 +67,34 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		log.Println(string(p))
+		log.Println("incoming message: ", string(p))
 
-		for _, value := range connections {
-			var rawMessage map[string]interface{}
-			json.Unmarshal(p, &rawMessage)
+		var output interface{}
+		var rawMessage map[string]interface{}
+		json.Unmarshal(p, &rawMessage)
+
+		if rawMessage["type"] == "action" {
+			var message ActionData
+			json.Unmarshal(p, &message)
+			log.Println("action full: ", message.Payload.Action)
+			if message.Payload.Action != "" {
+				gameLog = append(gameLog, message.Payload.Action)
+				log.Println("after adding action: ", gameLog)
+			}
+
+			output = GameLogOutput{
+				Type: "gameLog",
+				Payload: struct {
+					GameLog []string `json:"gameLog"`
+				}{GameLog: gameLog},
+			}
+		} else {
+			var message RegisterPlayerData
+			json.Unmarshal(p, &message)
+
 			if rawMessage["type"] == "registerPlayer" {
-				var message RegisterPlayerData
-				json.Unmarshal(p, &message)
 				players[myId] = message.Payload.Name
 			} else if rawMessage["type"] == "unregisterPlayer" {
-				var message RegisterPlayerData
-				json.Unmarshal(p, &message)
 				for unregisterKey, _ := range players {
 					if players[unregisterKey] == message.Payload.Name {
 						delete(players, unregisterKey)
@@ -73,21 +104,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 			playersSlice := []string{}
 			for _, value := range players {
+				log.Println(value)
 				playersSlice = append(playersSlice, value)
 			}
 
-			output := RegisteredPlayersOutput{
+			output = RegisteredPlayersOutput{
 				Type: "registeredPlayers",
 				Payload: struct {
 					Players []string `json:"players"`
 				}{Players: playersSlice},
 			}
+		}
 
-			out, _ := json.Marshal(output)
-			if err := value.WriteMessage(messageType, out); err != nil {
+		out, _ := json.Marshal(output)
+
+		for _, connValue := range connections {
+			if err := connValue.WriteMessage(messageType, out); err != nil {
 				log.Println(err, myId)
 				return
 			}
+			log.Println("outgoing message:", string(out))
 		}
 	}
 }
